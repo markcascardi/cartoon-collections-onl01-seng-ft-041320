c2074e540925925bf7f29ada4eab68fe94fabde6
Mark Cascardi <mcascardi@gmail.com>
2020-04-16 04:54:21 +0000

    Automatically backed up by Learn

--- spec/cartoon_collections_spec.rb
+++ spec/cartoon_collections_spec.rb
@@ -38,17 +38,17 @@ describe 'Cartoon Collections' do
     end
   end
 
-  # describe "#long_planeteer_calls" do
-  #   it "returns true if any calls are longer than 4 characters" do
-  #     calls_long = ["axe", "earth", "wind", "fire"]
-  #     expect(long_planeteer_calls(calls_long)).to eq(true)
-  #   end
-  #
-  #   it "returns false if all calls are 4 characters or less" do
-  #   calls_short = ["wind", "fire", "tree", "axe", "code"]
-  #   expect(long_planeteer_calls(calls_short)).to eq(false)
-  #   end
-  # end
+  describe "#long_planeteer_calls" do
+    it "returns true if any calls are longer than 4 characters" do
+      calls_long = ["axe", "earth", "wind", "fire"]
+      expect(long_planeteer_calls(calls_long)).to eq(true)
+    end
+
+    it "returns false if all calls are 4 characters or less" do
+    calls_short = ["wind", "fire", "tree", "axe", "code"]
+    expect(long_planeteer_calls(calls_short)).to eq(false)
+    end
+  end
 
   # describe '#find_the_cheese' do
   #   it 'returns the first element of the array that is cheese' do
